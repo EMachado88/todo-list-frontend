739ab150f84874c82ea8286ec931786ba485a7d3
Emanuel Machado <emanuel@emachado.pt>
2020-09-30 18:09:47 +0100

    Base project setup

---
+++ layouts/default.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      class="caption"
+      app
+    >
+      <span>&copy; Emanuel Machado {{ new Date().getFullYear() }}. All rights reserved.</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: false,
+      clipped: true,
+      fixed: true,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        }
+      ],
+      title: 'EDirect Insure TODO List'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="mb-2">
+        <v-card-title class="headline">
+          Welcome to the EDirect Insure TODO List
+        </v-card-title>
+      </v-card>
+      <v-card>
+        <v-card-text>
+          <p>Please login or register, if you don't already have an account.</p>
+          <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+          >
+            <v-text-field
+              v-model="email"
+              label="Email"
+              required
+            />
+
+            <v-text-field
+              v-model="password"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="showPassword ? 'text' : 'password'"
+              name="input-10-2"
+              label="Password"
+              hint="At least 6 characters"
+              class="input-group--focused mb-5"
+              @click:append="showPassword = !showPassword"
+            />
+
+            <v-btn
+              class="px-3 mr-2"
+              color="primary"
+              nuxt
+              to="/"
+            >
+              Login
+            </v-btn>
+            &nbsp;or
+            <v-btn
+              class="px-3 ml-3"
+              depressed
+              nuxt
+              to="/"
+            >
+              Register
+            </v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      valid: false,
+      email: '',
+      password: '',
+      showPassword: false
+    }
+  }
+}
+</script>
