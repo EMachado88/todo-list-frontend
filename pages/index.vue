c3ea60de5a92ed4ff4072d6f850e65ed117b9f42
Emanuel Machado <emanuel@emachado.pt>
2020-10-01 07:51:18 +0100

    Added "create new project"

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-row justify="center" align="center">
+  <v-row v-if="!user" justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <v-card class="mb-2">
         <v-card-title class="headline">
@@ -14,6 +14,48 @@
       </v-card>
     </v-col>
   </v-row>
+  <v-row v-else>
+    <v-col cols="4" sm="1" md="4">
+      <v-card>
+        <v-card-title class="headline">
+          Create a new project
+        </v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="projectName"
+            label="Project name"
+            required
+          />
+        </v-card-text>
+        <v-card-actions>
+          <v-btn block>
+            Create project
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+
+      <v-card
+        v-for="project in projects"
+        :key="project._id"
+      >
+        <v-card-title class="headline">
+          {{ project.name }}
+        </v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="projectName"
+            label="Project name"
+            required
+          />
+        </v-card-text>
+        <v-card-actions>
+          <v-btn block>
+            Create project
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -23,10 +65,26 @@ export default {
   components: {
     LoginForm
   },
+
   data () {
     return {
-      register: false
+      register: false,
+      projects: [],
+      projectName: ''
     }
+  },
+
+  computed: {
+    user () { return this.$store.state.user }
   }
+
+  // async mounted () {
+  //   const response = await this.$axios.get('api/projects', {
+  //     headers: {
+  //       Authorization: this.$store.state.token
+  //     }
+  //   })
+  //   console.log(response)
+  // }
 }
 </script>
