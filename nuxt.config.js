d79e3a3a4edede4f9840d793bd1e41044843c523
Emanuel Machado <emanuel@emachado.pt>
2020-10-01 07:47:52 +0100

    Updated app config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,11 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  mode: 'spa',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - EDirect Insure TODO List',
-    title: 'Home',
+    titleTemplate: '%s  -  EDirect Insure TODO List',
+    title: 'Dashboard',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -21,6 +22,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    { src: '~/plugins/vuex-persist', ssr: false }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
